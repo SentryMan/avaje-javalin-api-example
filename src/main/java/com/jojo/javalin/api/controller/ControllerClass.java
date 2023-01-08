82e4b5580fb5a029b1f3556b778fdf559f320721
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-01-08 15:16:35 -0500

    add openapi stuff

--- src/main/java/com/jojo/javalin/api/controller/ControllerClass.java
+++ src/main/java/com/jojo/javalin/api/controller/ControllerClass.java
@@ -1,16 +1,26 @@
 package com.jojo.javalin.api.controller;
 
+import com.jojo.javalin.api.exception.ErrorResponse;
 import com.jojo.javalin.api.service.ServiceClass;
 import io.avaje.http.api.Controller;
 import io.avaje.http.api.Get;
 import io.avaje.http.api.MediaType;
+import io.avaje.http.api.OpenAPIResponse;
 import io.avaje.http.api.Path;
 import io.avaje.http.api.Post;
 import io.avaje.http.api.Produces;
 import io.javalin.http.Context;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.inject.Inject;
 import java.util.List;
 
+@OpenAPIDefinition(
+    info =
+        @Info(
+            title = "Example service",
+            description = "Example Javalin controllers with Java and Maven"))
 @Controller
 @Path("javalin")
 public class ControllerClass {
@@ -23,8 +33,15 @@ public class ControllerClass {
     this.service = service;
   }
 
+  /**
+   * bog standard get
+   *
+   * @param b the body
+   * @return gets something a little sus
+   */
   @Get("/get")
-  // use regular Javalin Context
+  @Produces(MediaType.IMAGE_PNG)
+  @OpenAPIResponse(responseCode = "200", type = byte[].class)
   void ctxEndpoint(Context ctx) {
     System.out.println("Is Virtual Thread: " + Thread.currentThread().isVirtual());
     ctx.contentType("image/png").result(service.callDownStream());
@@ -38,9 +55,24 @@ public class ControllerClass {
     return "healthlmao";
   }
 
+  /**
+   * Standard Post
+   *
+   * @param b the body
+   * @return the response body (from javadoc)
+   */
+  @Deprecated
   @Post("/post")
-  RequestModel testPost(RequestModel m) {
-    System.out.println("got " + m);
+  @Tag(name = "tag1", description = "it's somethin")
+  @OpenAPIResponse(responseCode = "200", description = "from annotaion")
+  @OpenAPIResponse(responseCode = "201")
+  @OpenAPIResponse(responseCode = "400", description = "User not found")
+  @OpenAPIResponse(
+      responseCode = "500",
+      description = "Some other Error",
+      type = ErrorResponse.class)
+  RequestModel testPost(RequestModel b) {
+    System.out.println("got " + b);
     return new RequestModel("Hmmm, now that I think about it");
   }
 
@@ -49,5 +81,11 @@ public class ControllerClass {
 
     System.out.println("got " + m);
     return new RequestModel("This Model class isn't named that well");
+  }
+
+  @Get("/list")
+  List<RequestModel> testOpenApiList() {
+
+    return List.of(new RequestModel("it is what it is I guess"));
   }
 }
