f8cef05be4c8a7187d20e56a7796da7b3fcd7b4b
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-01-20 13:11:40 -0500

    add client factories

--- src/main/java/com/jojo/javalin/api/controller/clientserver/ClientServer.java
+++ src/main/java/com/jojo/javalin/api/controller/clientserver/ClientServer.java
@@ -6,11 +6,11 @@ import io.avaje.http.api.MediaType;
 import io.avaje.http.api.Path;
 import io.avaje.http.api.Produces;
 
-// double as a client interface
+// can double as a client interface
 // if in another maven module we'd use @Client.Import
 // implementing controllers can define own path annotation to override
 @Client
-@Path("somePath")
+@Path("")
 public interface ClientServer {
 
   /**
--- src/main/java/com/jojo/javalin/api/controller/clientserver/ExtendController.java
+++ src/main/java/com/jojo/javalin/api/controller/clientserver/ExtendController.java
@@ -4,8 +4,8 @@ import io.avaje.http.api.Get;
 import io.avaje.http.api.MediaType;
 import io.avaje.http.api.Path;
 import io.avaje.http.api.Produces;
-// path is inherited by all implementing classes
 
+// path is inherited by all implementing classes
 @Path("javalin")
 public interface ExtendController {
 
