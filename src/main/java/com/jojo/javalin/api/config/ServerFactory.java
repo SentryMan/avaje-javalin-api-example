a8457def0acb085459ad0a1febe4a1d79834b9c4
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-01-30 21:51:38 -0500

    autoprovide jsonb

--- src/main/java/com/jojo/javalin/api/config/ServerFactory.java
+++ src/main/java/com/jojo/javalin/api/config/ServerFactory.java
@@ -3,7 +3,6 @@ package com.jojo.javalin.api.config;
 import io.avaje.http.api.WebRoutes;
 import io.avaje.inject.Bean;
 import io.avaje.inject.Factory;
-import io.avaje.jsonb.Jsonb;
 import io.javalin.Javalin;
 import java.util.List;
 
@@ -18,10 +17,5 @@ public class ServerFactory {
     customizers.forEach(c -> c.customize(app));
 
     return app;
-  }
-
-  @Bean
-  Jsonb jsonB() {
-    return Jsonb.builder().failOnUnknown(false).build();
   }
 }
